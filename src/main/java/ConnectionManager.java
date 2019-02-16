6c7cb2e42af41912978ba1a52e3a3b3ee529571b
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2019-02-16 21:33:34 +0200

    ConnectionManager can: -clear -create -getTableData

--- src/main/java/ConnectionManager.java
+++ src/main/java/ConnectionManager.java
@@ -39,4 +39,78 @@ public class ConnectionManager {
             return new String[0];
         }
     }
+
+    public void clear(String tableName){
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("DELETE  FROM "+ tableName);
+            stmt.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void create(DataSet input) {
+        try {
+            Statement stmt = connection.createStatement();
+            String tableNames = getNameFormated(input, "%s,");
+            String values = getValuesFormated(input,"'%s',");
+           String sql = "INSERT INTO public.users (" + tableNames + ")" + "VALUES (" + values + ")";
+            stmt.execute(sql);
+            stmt.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private String getValuesFormated(DataSet input, String format) {
+        String values = "";
+        for (Object value : input.getValues()) {
+            values += String.format(format, value);
+        }
+        values = values.substring(0, values.length()-1);
+        return values;
+    }
+
+    private String getNameFormated(DataSet newValue, String format) {
+        String string = "";
+        for (String name : newValue.getNames()) {
+            string += String.format(format, name);
+        }
+        string = string.substring(0, string.length()-1);
+        return string;
+    }
+
+    public DataSet[] getTableData(String tableName) {
+        try {
+            int size = getSize(tableName);
+
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT  * FROM "+ tableName);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            DataSet[] result = new DataSet[size];
+            int index = 0;
+            while (rs.next()){
+                DataSet dataSet = new DataSet();
+                result[index++] = dataSet;
+                for (int i = 1; i <= rsmd.getColumnCount(); i++){
+                    dataSet.put(rsmd.getColumnName(i), rs.getObject(i));
+                }
+            }
+            rs.close();
+            stmt.close();
+            return result;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return new DataSet[0];
+        }
+    }
+
+    private int getSize(String tableName) throws SQLException {
+        Statement stmt = connection.createStatement();
+        ResultSet rsCount = stmt.executeQuery("SELECT COUNT(*) FROM " + tableName);
+        rsCount.next();
+        int size = rsCount.getInt(1);
+        return size;
+    }
 }
--- src/main/test/ConnectionManagerTest.java
+++ src/main/test/ConnectionManagerTest.java
@@ -10,14 +10,31 @@ public class ConnectionManagerTest {
     private ConnectionManager manager;
 
     @Before
-    public void setup(){
+    public void setup() {
         manager = new ConnectionManager();
         manager.connect("anytest", "postgres", "post");
     }
 
     @Test
-    public void test_get_all_table_names(){
+    public void test_get_all_table_names() {
         String[] tableNames = manager.getTableNames();
         assertEquals("[users, cars]", Arrays.toString(tableNames));
+    }
+
+    @Test
+    public void test_get_table_data() {
+        manager.clear("users");
+        DataSet input = new DataSet();
+        input.put("id", 13);
+        input.put("name", "Ivan");
+        input.put("email", "ivan@gmail.com");
+        manager.create(input);
+
+        DataSet[] sets = manager.getTableData("users");
+        assertEquals(1, sets.length);
+
+        DataSet user = sets[0];
+        assertEquals("[13, Ivan, ivan@gmail.com]", Arrays.toString(user.getValues()));
+        assertEquals("[id, name, email]", Arrays.toString(user.getNames()));
     }
 }
