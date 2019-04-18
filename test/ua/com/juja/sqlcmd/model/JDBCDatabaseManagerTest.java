2345713b3f62aee8ce784a5feba9a2532accee2a
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2019-04-18 21:06:24 +0300

    add test jdbc layer

--- test/ua/com/juja/sqlcmd/model/JDBCDatabaseManagerTest.java
+++ test/ua/com/juja/sqlcmd/model/JDBCDatabaseManagerTest.java
@@ -7,60 +7,10 @@ import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
-public class JDBCDatabaseManagerTest {
+public class JDBCDatabaseManagerTest extends DatabaseManagerTest {
 
-    private JDBCDatabaseManager manager;
-
-    @Before
-    public void setup() {
-        manager = new JDBCDatabaseManager();
-        manager.connect("anytest", "postgres", "post");
-    }
-
-    @Test
-    public void test_get_all_table_names() {
-        String[] tableNames = manager.getTableNames();
-        assertEquals("[users, cars]", Arrays.toString(tableNames));
-    }
-
-    @Test
-    public void test_get_table_data() {
-        manager.clear("users");
-        DataSet input = new DataSet();
-        input.put("id", 13);
-        input.put("name", "Ivan");
-        input.put("email", "ivan@gmail.com");
-        manager.create("users", input);
-
-        DataSet[] sets = manager.getTableData("users");
-        assertEquals(1, sets.length);
-
-        DataSet user = sets[0];
-        assertEquals("[13, Ivan, ivan@gmail.com]", Arrays.toString(user.getValues()));
-        assertEquals("[id, name, email]", Arrays.toString(user.getNames()));
-    }
-
-    @Test
-    public void testUpdateTableData() {
-        manager.clear("users");
-        DataSet input = new DataSet();
-        input.put("id", 13);
-        input.put("name", "Ivan");
-        input.put("email", "ivan@gmail.com");
-        manager.create("users", input);
-
-        // when
-        DataSet newValue = new DataSet();
-        newValue.put("name", "Ivanupdater");
-        newValue.put("email", "ivan_update@gmail.com");
-        manager.update("users", 13, newValue);
-
-        // then
-        DataSet[] users = manager.getTableData("users");
-        assertEquals(1, users.length);
-
-        DataSet user = users[0];
-        assertEquals("[id, name, email]", Arrays.toString(user.getNames()));
-        assertEquals("[13, Ivanupdater, ivan_update@gmail.com]", Arrays.toString(user.getValues()));
+    @Override
+    public DatabaseManager getDatabaseManager(){
+        return new JDBCDatabaseManager();
     }
 }
