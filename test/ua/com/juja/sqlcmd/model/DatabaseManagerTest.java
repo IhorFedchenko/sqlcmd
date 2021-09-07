6376c4d0a4136a6c1f5b512d88f3e171f373d508
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2021-09-07 16:57:54 +0300

    fix test

--- test/ua/com/juja/sqlcmd/model/DatabaseManagerTest.java
+++ test/ua/com/juja/sqlcmd/model/DatabaseManagerTest.java
@@ -32,7 +32,7 @@ public abstract class DatabaseManagerTest {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        assertEquals("[cars, ISO639-3, users]", tableNames.toString());
+        assertEquals("[cars, languages, users]", tableNames.toString());
     }
 
     @Test
